317a7462b924097936112217601413dbb8d803cf
pshahapur <prashant.shahapur@gmail.com>
2018-11-13 14:03:41 +0530

    Base Code

--- asn-datasource-microservice-server/src/main/java/com/syne/asn/datasource/constant/AsnConstants.java
+++
@@ -1,7 +0,0 @@
-package com.syne.asn.datasource.constant;
-
-public class AsnConstants {
-	public final static String IFRS = "Ifrs";
-	public final static String GAAP = "Gaap";
-	public final static String BASEL = "Basel";
-}
